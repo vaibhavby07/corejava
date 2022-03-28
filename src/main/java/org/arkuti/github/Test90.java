2f0ec12a379d0fa0da1b73a9a2b31312fc2195e1
vaibhav <vaibhav@Vaibhav>
2022-03-28 08:45:50 +0530

    share simple addition project

---
+++ src/main/java/org/arkuti/github/Test90.java
@@ -0,0 +1,10 @@
+package org.arkuti.github;
+
+public class Test90 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/github/Test2.java
@@ -0,0 +1,12 @@
+package github;
+
+public class Test2
+{
+	public static void main(String[] args) 
+	{
+       int a=10;
+	   int b=20;
+	   System.out.println("addition of two number (a+b)");
+	   System.out.println("10+20");
+	}
+}
